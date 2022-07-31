16ba6573d3cff2bf04dbf693278fcd7b13680c41
Kryštof <krystof.kratky2003@gmail.com>
2022-07-31 18:19:03 +0000

    sb theme

---
+++ .storybook/theme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming';
+
+export default create({
+    base: 'light',
+    brandTitle: 'Histories storybook',
+    brandUrl: 'https://histories.cc',
+    brandImage: 'https://github.com/histories-cc/histories/blob/main/public/logo/big-black.svg?raw=true',
+    brandTarget: '_self',
+});
