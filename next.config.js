906cc9f1dcbdd313a919b4a085baff0f9510a06d
Kryštof <krystof.kratky2003@gmail.com>
2022-08-08 18:06:28 +0000

    storybook redirect fix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,20 @@
 module.exports = {
   reactStrictMode: true,
 
+  async redirects() {
+    return [
+      {
+        source: "/storybook",
+        destination: "/storybook/index.html",
+        permanent: true,
+      },
+    ];
+  },
+
   images: {
     minimumCacheTTL: 60,
     domains: ['i.pravatar.cc', 'external-content.duckduckgo.com'],
   },
+
+
 };
